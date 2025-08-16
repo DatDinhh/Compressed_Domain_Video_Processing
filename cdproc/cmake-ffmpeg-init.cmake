1c14c9e70bc06b5f2241da6e6eb5211c4f65adb7
Dat Dat <dat.hbdinh@gmail.com>
2025-08-16 00:51:05 -0400

    Create cmake-ffmpeg-init.cmake

---
+++ cdproc/cmake-ffmpeg-init.cmake
@@ -0,0 +1,3 @@
+set(FFMPEG_FOUND ON CACHE BOOL "preseeded")
+set(FFMPEG_INCLUDE_DIR "C:/ffmpeg/ffmpeg-7.1.1-essentials_build/include" CACHE PATH "ffmpeg include")
+set(FFMPEG_LIBRARIES "C:/ffmpeg/ffmpeg-7.1.1-essentials_build/lib/avcodec.lib;C:/ffmpeg/ffmpeg-7.1.1-essentials_build/lib/avformat.lib;C:/ffmpeg/ffmpeg-7.1.1-essentials_build/lib/avutil.lib;C:/ffmpeg/ffmpeg-7.1.1-essentials_build/lib/swscale.lib;C:/ffmpeg/ffmpeg-7.1.1-essentials_build/lib/swresample.lib" CACHE STRING "ffmpeg libs")
